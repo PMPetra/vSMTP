760afc3425f6fefed14e260443d836f8ca082ca0
Mathieu-Lala <m.lala@viridit.com>
2022-05-12 21:08:28 +0900

    test: diff work with vsmtp

---
+++ tools/diff/README.md
@@ -0,0 +1,9 @@
+# Compare SMTP servers
+
+## Usage
+
+```sh
+$> sudo -v && ./tools/diff/diff.sh `user`
+```
+
+The `user` parameter is used to run `smtp-sink`
--- tools/diff/diff.sh
+++ tools/diff/diff.sh
@@ -2,47 +2,60 @@
 
 set -e
 
-# systemctl stop vsmtp || true
-# systemctl disable vsmtp || true
-# systemctl enable postfix
-#
-# /home/lala/.cargo/bin/hyperfine \
-#     -L mail 1,10 \
-#     -L session 1,10 \
-#     --prepare 'systemctl restart postfix' \
-#     'smtp-source -s {session} -l 5120 -m {mail} -c -f to@local.com -N -t postfix@smtp-sink localhost:25' \
-#     --export-json diff-postfix.json \
-#     --show-output
+metrics_vsmtp() {
+    export PATH=$PATH:./target/release/
+    user=$1
 
-# systemctl stop postfix || true
-# systemctl disable postfix || true
+    pkill vsmtp || true
+    pkill smtp-sink || true
 
+    # FIXME:
+    # The (-M $count)-nth transaction received by `smtp-sink` does not receive
+    # a SMTP codes after the `<CRLF>.<CRLF>` command, so *only* the last mail
+    # produce a "response error: incomplete response"
 
-if pgrep vsmtp; then pkill vsmtp; fi
-if pgrep smtp-sink; then pkill smtp-sink; fi
+    hyperfine                   \
+        --runs 5                \
+        -L mail 1,10,100        \
+        -L session 1,10,100     \
+        --setup 'rm -rf ./tools/diff/generated/spool && vsmtp -c ./tools/diff/vsmtp.toml --no-daemon & sleep 1'         \
+        -n 'vsmtp s={session}&m={mail}'  \
+        'sudo smtp-sink -M {mail} -u '${user}' -d /tmp/smtp-sink-output/ 127.0.0.1:25 1 &     \
+        smtp-source -s {session} -l 5120 -m {mail} -f from@smtp-source -N -t vsmtp@smtp-sink localhost:10025'     \
+        --cleanup       \
+            'pkill vsmtp &&                 \
+            rm -rf ./diff/generated'        \
+        --export-json diff-vsmtp.json
+}
 
+metrics_potfix() {
+    user=$1
 
-export PATH=$PATH:/home/lala/.cargo/bin/:./target/release/
+    pkill smtp-sink || true
 
-hyperfine           \
-    --runs 1        \
-    -L mail 1       \
-    -L session 1    \
-    --setup 'rm -rf ./tools/diff/generated/spool && vsmtp -c ./tools/diff/vsmtp.toml --no-daemon & sleep 1'     \
-    'sudo smtp-sink -M {mail} -v -c -u lala 127.0.0.1:25 1 &     \
-    smtp-source -s {session} -l 5120 -m {mail} -c -f from@smtp-source -N -t vsmtp@smtp-sink localhost:10025 &       \
-    wait $(pgrep vsmtp-sink) $(pgrep smtp-source)'           \
-    --cleanup       \
-        'pkill vsmtp &&                  \
-        rm -rf ./diff/generated'        \
-    --export-json diff-vsmtp.json       \
-    --show-output
+    systemctl enable postfix
 
-## how to access function `cleanup` in --cleanup command ?
+    hyperfine --runs 5  \
+        -L mail 2        \
+        -L session 1     \
+        --setup 'systemctl restart postfix & sleep 1'         \
+        -n 'postfix s={session}&m={mail}'  \
+        'sudo smtp-sink -c -v -M {mail} -u '${user}' -d /tmp/smtp-sink-output/ 127.0.0.1:10025 1 &     \
+        smtp-source -c -v -s {session} -l 5120 -m {mail} -f from@smtp-source -N -t vsmtp@smtp-sink localhost:25'     \
+        --export-json diff-postfix.json   \
+        --show-output
 
-jq -s '.[0].results=([.[].results]|flatten)|.[0]' diff-postfix.json diff-vsmtp.json > diff.json
+    systemctl disable postfix
+}
+
+rm -rf /tmp/smtp-sink-output/
+
+metrics_vsmtp $1
+# metrics_potfix $1
+
+jq -s '.[0].results=([.[].results]|flatten)|.[0]' diff-vsmtp.json > diff.json
 rm -f ./diff-vsmtp.json
-rm -f ./diff-postfix.json
+# rm -f ./diff-postfix.json
 
 ## You can then visualize the data with `https://github.com/sharkdp/hyperfine/tree/master/scripts`
 
