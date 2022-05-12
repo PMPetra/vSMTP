06dc19b709082b95a72b236b6915386049ad484c
Mathieu-Lala <m.lala@viridit.com>
2022-05-12 22:45:00 +0900

    test: add diff for other mta

--- tools/diff/diff.sh
+++ tools/diff/diff.sh
@@ -21,7 +21,7 @@ metrics_vsmtp() {
         --setup 'rm -rf ./tools/diff/generated/spool && vsmtp -c ./tools/diff/vsmtp.toml --no-daemon & sleep 1'         \
         -n 'vsmtp s={session}&m={mail}'  \
         'sudo smtp-sink -M {mail} -u '${user}' -d /tmp/smtp-sink-output/ 127.0.0.1:25 1 &     \
-        smtp-source -s {session} -l 5120 -m {mail} -f from@smtp-source -N -t vsmtp@smtp-sink localhost:10025'     \
+        smtp-source -s {session} -l 5120 -m {mail} -f vsmtp@smtp-source -N -t vsmtp@smtp-sink localhost:10025'     \
         --cleanup       \
             'pkill vsmtp &&                 \
             rm -rf ./diff/generated'        \
@@ -35,15 +35,14 @@ metrics_potfix() {
 
     systemctl enable postfix
 
-    hyperfine --runs 5  \
-        -L mail 2        \
-        -L session 1     \
-        --setup 'systemctl restart postfix & sleep 1'         \
-        -n 'postfix s={session}&m={mail}'  \
-        'sudo smtp-sink -c -v -M {mail} -u '${user}' -d /tmp/smtp-sink-output/ 127.0.0.1:10025 1 &     \
-        smtp-source -c -v -s {session} -l 5120 -m {mail} -f from@smtp-source -N -t vsmtp@smtp-sink localhost:25'     \
-        --export-json diff-postfix.json   \
-        --show-output
+    hyperfine --runs 5          \
+        -L mail 1,10,100        \
+        -L session 1,10,100     \
+        --setup 'systemctl restart postfix & sleep 3'   \
+        -n 'postfix s={session}&m={mail}'               \
+        'sudo smtp-sink -M {mail} -u '${user}' -d /tmp/smtp-sink-output/ 127.0.0.1:10025 1 &     \
+        smtp-source -s {session} -l 5120 -m {mail} -f postfix@smtp-source -N -t postfix@smtp-sink 127.0.0.1:25'     \
+        --export-json diff-postfix.json
 
     systemctl disable postfix
 }
@@ -51,11 +50,11 @@ metrics_potfix() {
 rm -rf /tmp/smtp-sink-output/
 
 metrics_vsmtp $1
-# metrics_potfix $1
+metrics_potfix $1
 
-jq -s '.[0].results=([.[].results]|flatten)|.[0]' diff-vsmtp.json > diff.json
+jq -s '.[0].results=([.[].results]|flatten)|.[0]' diff-vsmtp.json diff-postfix.json > diff.json
 rm -f ./diff-vsmtp.json
-# rm -f ./diff-postfix.json
+rm -f ./diff-postfix.json
 
 ## You can then visualize the data with `https://github.com/sharkdp/hyperfine/tree/master/scripts`
 
